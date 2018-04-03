51a31083115869fbbc0b2665b08685c9068bc73e
Patrick Niepel <patrick.niepel@hof-university.de>
2018-04-03 10:53:51 +0200

    test

---
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -0,0 +1,15 @@
+package org.leanpoker.player;
+
+public class GameState {
+	
+	public int round;
+	public int bet_index;
+	public int small_blind;
+	public int current_buy_in;
+	public int pot;
+	public int minimum_raise;
+	public int dealer;
+	public int orbits;
+	public int in_action;
+	
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,7 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
@@ -9,7 +11,17 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 20;
+    	
+    	//Gson gson = new Gson(); // Or use new GsonBuilder().create();
+    	Gson gson = new Gson();
+    	
+    	//String json = gson.toJson); // serializes target to Json
+    	GameState state = gson.fromJson(request, GameState.class); // deserializes json into target2
+    	
+    	
+        
+    	 
+    	 return state.current_buy_in;
     }
 
     public static void showdown(JsonElement game) {
